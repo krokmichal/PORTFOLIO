0ba82d25ae3a77fa28c0b0b1f08943f6ebc094ff
Michal Krok <krokmichal1@gmail.com>
2023-11-24 18:36:29 +0100

    New features added

--- hangman/assets/js/main.js
+++
@@ -1,8 +0,0 @@
-document.querySelector('#newGame').addEventListener('click', startGame)
-
-let clickSound = new Audio('/sounds/click.wav');
-
-function startGame() {
-    clickSound.play();
-    window.location.href = "/assets/html/gameOptions.html";
-}
